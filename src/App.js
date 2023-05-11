16e9593f553e9c7ce0e96ba26d111cff3079a864
JundaJagad <121542411+JundaJagad@users.noreply.github.com>
2023-05-11 21:51:49 +0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import UploadForm from './UploadForm';
+
+function App() {
+  return (
+    <div>
+      <h1 className='center'>Formulir Pengunggahan Berkas</h1>
+      <UploadForm />
+    </div>
+  );
+}
+
+export default App;
+
